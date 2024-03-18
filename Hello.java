8fdea074d14de3576ca56da97901f8b1aa685c9d
aprateek47 <163670634+aprateek47@users.noreply.github.com>
2024-03-18 12:58:05 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println(".....**** Welcome TO Jenkins Training*****.....");
+		System.out.println(".....**** Made some changes *****.....");
 }
 }
